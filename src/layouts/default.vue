b4878f73613330a236ac275165077021df482d0b
ozelotjp <contact@ozelot.app>
2020-03-11 08:40:08 +0000

    feat: layoutsのdefaultページを作成

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,19 +1,104 @@
 <template>
   <v-app>
+    <v-navigation-drawer v-model="state.drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>
+            ShareBank
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            ver1.0.0
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider />
+      <v-list dense nav>
+        <v-list-item
+          v-for="(item, index) in navigationMenu"
+          :key="index"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>
+              {{ item.icon }}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            {{ item.text }}
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app>
-      <v-icon>
-        mdi-domain
-      </v-icon>
-      <v-toolbar-title to="/">
-        ShareBank
-      </v-toolbar-title>
+      <v-app-bar-nav-icon @click="state.drawer = !state.drawer" />
       <v-spacer />
-      <v-btn text to="/mypage">
-        マイページ
-      </v-btn>
+      <v-menu offset-y bottom left>
+        <template v-slot:activator="{ on }">
+          <v-btn text class="text-none" v-on="on">
+            {{ currentBank.text }}
+            <v-icon right>
+              mdi-menu-down
+            </v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item
+            v-for="bank in bankList"
+            :key="bank.id"
+            :to="'/bank/' + bank.id"
+          >
+            <v-list-item-title>
+              {{ bank.text }}
+            </v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-content>
       <nuxt />
     </v-content>
   </v-app>
 </template>
+
+<script lang="ts">
+import { reactive, defineComponent } from '@vue/composition-api'
+
+interface NavigationMenu {
+  icon: string
+  text: string
+  to: string
+}
+
+interface BankList {
+  id: string
+  text: string
+}
+
+export default defineComponent({
+  setup() {
+    const state = reactive({
+      drawer: true
+    })
+
+    const navigationMenu = [
+      { icon: 'mdi-home', text: 'Home', to: '/' }
+    ] as NavigationMenu[]
+
+    const bankList = [
+      { id: '1st', text: '1st Bank' },
+      { id: '2nd', text: '2nd Bank' },
+      { id: '3rd', text: '3rd Bank' }
+    ] as BankList[]
+
+    const currentBank = { id: '1st', text: '1st Bank' } as BankList
+
+    return {
+      state,
+      navigationMenu,
+      bankList,
+      currentBank
+    }
+  }
+})
+</script>
