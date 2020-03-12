3871774d5640a281382da7c1436b522dacbf3e11
ozelotjp <contact@ozelot.app>
2020-03-12 05:31:57 +0000

    chore: vuex-module-decoratorsに必要なファイルを追加

---
+++ src/store/index.ts
@@ -0,0 +1,5 @@
+import { Store } from 'vuex'
+import { initializeStores } from '~/utils/store-accessor.ts'
+const initialaizer = (store: Store<any>) => initializeStores(store)
+export const plugins = [initialaizer]
+export * from '~/utils/store-accessor.ts'
---
+++ src/utils/store-accessor.ts
@@ -0,0 +1,9 @@
+import { Store } from 'vuex'
+import { getModule } from 'vuex-module-decorators'
+
+/* eslint-disable-next-line import/no-mutable-exports */
+/* eslint-enable */
+
+function initializeStores(store: Store<any>): void { }
+
+export { initializeStores }
