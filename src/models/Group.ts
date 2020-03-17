702570f0d0befd5a608945a6311d61353ff60974
ozelotjp <contact@ozelot.app>
2020-03-17 08:24:32 +0000

    chore: groupモデルを追加

---
+++ src/models/Group.ts
@@ -0,0 +1,21 @@
+export interface IGroupDocumentData {
+  id: string
+  updatedAt: firebase.firestore.Timestamp
+  user: {
+    [uid: string]: {
+      name: string
+      role: 'owner' | 'moderator' | 'write' | 'read'
+      wallet: number
+    }
+  }
+}
+
+export interface IGroupHistoryDocumentData {
+  id: string
+  author: string
+  change: {
+    [uid: string]: number
+  }
+  createdAt: firebase.firestore.Timestamp
+  title: string
+}
