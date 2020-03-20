c23e1ed42aba1a9cf1b6dd67b50f2c80025096ce
ozelotjp <contact@ozelot.app>
2020-03-20 16:47:56 +0900

    chore: current機能を削除

--- src/store/group.ts
+++ src/store/group.ts
@@ -4,13 +4,8 @@ import { IGroupDocumentData } from '~/models/Group'
 @Module({ name: 'group', namespaced: true, stateFactory: true })
 export default class Group extends VuexModule {
   ready = false
-  current = '' as string
   list = [] as IGroupDocumentData[]
 
-  get currentGroup(): IGroupDocumentData {
-    return this.list.filter(
-      (group: IGroupDocumentData) => group.id === this.current
-    )[0]
   }
 
   @Mutation
@@ -19,18 +14,8 @@ export default class Group extends VuexModule {
     this.ready = true
   }
 
-  @Mutation
-  SET_CURRENT_GROUP(id: string) {
-    this.current = id
-  }
-
   @Action
   initializeGroup(list: IGroupDocumentData[]) {
     this.INITIALIZE_GROUP(list)
-  }
-
-  @Action
-  selectGroup(id: string) {
-    this.SET_CURRENT_GROUP(id)
   }
 }
