14ce54d826da2db811c0ff5d5bad8a3d6a96eda7
ozelotjp <contact@ozelot.app>
2020-03-27 07:03:28 +0000

    feat: convertTimestampToDateTimeFormatを追加

--- src/utils/format-data.ts
+++ src/utils/format-data.ts
@@ -1,6 +1,24 @@
-export const convertDateToISO8601Format = (date: Date): string => {
+import Firebase from 'firebase'
+
+export const convertTimestampToDateFormat = (
+  timestamp: Firebase.firestore.Timestamp
+): string => {
+  const date = timestamp.toDate()
   const y = date.getFullYear()
   const m = ('0' + (date.getMonth() + 1)).slice(-2)
   const d = ('0' + date.getDay()).slice(-2)
   return `${y}-${m}-${d}`
+}
+
+export const convertTimestampToDateTimeFormat = (
+  timestamp: Firebase.firestore.Timestamp
+): string => {
+  const date = timestamp.toDate()
+  const y = date.getFullYear()
+  const m = ('0' + (date.getMonth() + 1)).slice(-2)
+  const d = ('0' + date.getDay()).slice(-2)
+  const h = ('0' + date.getHours()).slice(-2)
+  const i = ('0' + date.getMinutes()).slice(-2)
+  const s = ('0' + date.getSeconds()).slice(-2)
+  return `${y}-${m}-${d} ${h}:${i}:${s}`
 }
