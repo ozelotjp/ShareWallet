ff152471bc6ae8a9dd1c131b09ffeba2d6465d7d
ozelotjp <contact@ozelot.app>
2020-04-12 09:13:40 +0000

    fix: 日付を代入すべきところが曜日になっていた

--- src/utils/format-data.ts
+++ src/utils/format-data.ts
@@ -6,7 +6,7 @@ export const convertTimestampToDateFormat = (
   const date = timestamp.toDate()
   const y = date.getFullYear()
   const m = ('0' + (date.getMonth() + 1)).slice(-2)
-  const d = ('0' + date.getDay()).slice(-2)
+  const d = ('0' + date.getDate()).slice(-2)
   return `${y}-${m}-${d}`
 }
 
@@ -16,7 +16,7 @@ export const convertTimestampToDateTimeFormat = (
   const date = timestamp.toDate()
   const y = date.getFullYear()
   const m = ('0' + (date.getMonth() + 1)).slice(-2)
-  const d = ('0' + date.getDay()).slice(-2)
+  const d = ('0' + date.getDate()).slice(-2)
   const h = ('0' + date.getHours()).slice(-2)
   const i = ('0' + date.getMinutes()).slice(-2)
   const s = ('0' + date.getSeconds()).slice(-2)
