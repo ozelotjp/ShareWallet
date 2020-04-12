1da06d4272ba08b79099509b0a6dbf38f823c513
ozelotjp <contact@ozelot.app>
2020-04-12 09:41:51 +0000

    feat: groupページを作成

--- src/pages/group/_id.vue
+++ src/pages/group/_id.vue
@@ -14,7 +14,7 @@
             <template v-slot:top>
               <v-toolbar flat>
                 <v-toolbar-title>
-                  残高：{{ group.users[uid].wallet }}円
+                  残高：{{ group.users[myUid].wallet }}円
                 </v-toolbar-title>
                 <v-spacer />
                 <v-btn @click="showAddTransactionDialog()">
@@ -22,9 +22,13 @@
                 </v-btn>
               </v-toolbar>
             </template>
-            <template v-slot:item.change="{ item }">
-              <v-chip :color="item.change >= 0 ? 'green' : 'red'" dark>
-                {{ item.change >= 0 ? '+' : '' }}{{ item.change }}
+            <template v-slot:item.diff="{ item }">
+              <v-chip
+                v-if="typeof item.diff !== 'undefined'"
+                :color="item.diff >= 0 ? 'green' : 'red'"
+                dark
+              >
+                {{ item.diff >= 0 ? '+' : '' }}{{ item.diff }}
               </v-chip>
             </template>
             <template v-slot:item.actions="{ item }">
@@ -36,7 +40,11 @@
         </v-card>
       </v-col>
     </v-row>
-    <v-dialog v-model="historyDialogModal" max-width="512">
+    <v-dialog
+      v-if="historyDialogModal"
+      v-model="historyDialogModal"
+      max-width="512"
+    >
       <v-card>
         <v-card-title>
           詳細
@@ -58,7 +66,7 @@
           />
           <v-text-field
             label="作成者"
-            :value="historyDialog.author"
+            :value="group.users[historyDialog.author].name"
             prepend-inner-icon="mdi-account"
             outlined
             readonly
@@ -78,13 +86,13 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="user in historyDialog.users" :key="user.id">
+              <tr v-for="user in historyDialog.users" :key="user.uid">
                 <td>
-                  {{ user.name }}
+                  {{ group.users[user.uid].name }}
                 </td>
                 <td>
-                  <v-chip :color="user.change >= 0 ? 'green' : 'red'" dark>
-                    {{ user.change >= 0 ? '+' : '' }}{{ user.change }}
+                  <v-chip :color="user.diff >= 0 ? 'green' : 'red'" dark>
+                    {{ user.diff >= 0 ? '+' : '' }}{{ user.diff }}
                   </v-chip>
                 </td>
                 <td>
@@ -105,7 +113,11 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-    <v-dialog v-model="addTransactionDialogModal" max-width="512">
+    <v-dialog
+      v-if="addTransactionDialogModal"
+      v-model="addTransactionDialogModal"
+      max-width="512"
+    >
       <v-card>
         <v-card-title>
           取引を追加
@@ -116,8 +128,8 @@
         </v-card-title>
         <v-card-text>
           <v-text-field
+            v-model="addTransactionDialog.title"
             label="取引内容"
-            :value="addTransactionDialog.title"
             prepend-inner-icon="mdi-text"
             outlined
             placeholder="昼飯代など"
@@ -137,22 +149,22 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="user in addTransactionDialog.users" :key="user.id">
+              <tr v-for="user in addTransactionDialog.users" :key="user.uid">
                 <td>
-                  {{ user.name }}
+                  {{ group.users[user.uid].name }}
                 </td>
                 <td>
                   <v-text-field
-                    v-model="user.change"
+                    v-model="user.diff"
                     append-icon="mdi-currency-jpy"
-                    style="max-width: 150px"
+                    style="max-width: 150px;"
                     reverse
                   >
                     0
                   </v-text-field>
                 </td>
                 <td>
-                  <v-btn @click="adjustDifference(user.id)">
+                  <v-btn @click="adjustDifference(user.uid)">
                     差額調整
                   </v-btn>
                 </td>
@@ -165,7 +177,10 @@
           <v-btn @click="addTransactionDialogModal = false">
             キャンセル
           </v-btn>
-          <v-btn @click="addTransaction()">
+          <v-btn
+            :loading="addTransactionDialogLoading"
+            @click="addTransaction()"
+          >
             取引を追加
           </v-btn>
         </v-card-actions>
@@ -176,7 +191,9 @@
 
 <script lang="ts">
 import { defineComponent, reactive, ref, computed } from '@vue/composition-api'
-import { IGroupHistoryDocumentData, IGroupDocumentData } from '@/models/Group'
+import { IAddTransaction } from '@@/models/AddTransaction'
+import { IGroupDocumentData } from '@@/models/GroupDocument'
+import { IGroupHistoryDocumentData } from '@@/models/GroupHistoryDocument'
 import {
   convertTimestampToDateFormat,
   convertTimestampToDateTimeFormat
@@ -185,59 +202,66 @@ import {
 export default defineComponent({
   middleware: 'authenticated',
   setup(_, { root: { $firebase, $route } }) {
-    const id = $route.params.id
-    const uid = $firebase.auth().currentUser!.uid
+    // general
+    const hitoryId = $route.params.id
+    const myUid = $firebase.auth().currentUser!.uid
     const ready = reactive({
       wallet: false
     })
 
+    // group, group-histories
     const group = reactive({} as IGroupDocumentData)
-    $firebase
-      .firestore()
-      .collection('group')
-      .doc(id)
-      .get()
-      .then((document) => {
-        const groupDocument = Object.assign(document.data(), {
-          id: document.id
-        }) as IGroupDocumentData
-        group.id = groupDocument.id
-        group.title = groupDocument.title
-        group.updatedAt = groupDocument.updatedAt
-        group.users = groupDocument.users
-        ready.wallet = true
-      })
-
     const histories = ref([] as IGroupHistoryDocumentData[])
-    $firebase
-      .firestore()
-      .collection('group')
-      .doc(id)
-      .collection('histories')
-      .orderBy('createdAt', 'desc')
-      .get()
-      .then((documentsQuery) => {
-        documentsQuery.docs.forEach((document) => {
-          histories.value.push(
-            Object.assign(document.data(), {
-              id: document.id
-            }) as IGroupHistoryDocumentData
-          )
+    const updateGroupState = () => {
+      ready.wallet = false
+      $firebase
+        .firestore()
+        .collection('group')
+        .doc(hitoryId)
+        .get()
+        .then((document) => {
+          const groupDocument = Object.assign(document.data(), {
+            id: document.id
+          }) as IGroupDocumentData
+          group.id = groupDocument.id
+          group.title = groupDocument.title
+          group.updatedAt = groupDocument.updatedAt
+          group.users = groupDocument.users
         })
-      })
+      $firebase
+        .firestore()
+        .collection('group')
+        .doc(hitoryId)
+        .collection('histories')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((documentsQuery) => {
+          histories.value = []
+          documentsQuery.docs.forEach((document) => {
+            histories.value.push(
+              Object.assign(document.data(), {
+                id: document.id
+              }) as IGroupHistoryDocumentData
+            )
+          })
+          ready.wallet = true
+        })
+    }
+    updateGroupState()
 
+    // histories table
     interface IHistoriesTableItems {
       id: string
       createdAt: string
       title: string
-      change: number
-      wallet: number
+      diff: number | undefined
+      wallet: number | undefined
     }
     const historiesTable = computed(() => {
       const headers = [
         { text: '日付', value: 'createdAt' },
         { text: '取引内容', value: 'title' },
-        { text: '金額', value: 'change' },
+        { text: '金額', value: 'diff' },
         { text: '差引残高', value: 'wallet' },
         { value: 'actions', width: '1%' }
       ]
@@ -247,8 +271,8 @@ export default defineComponent({
           id: history.id,
           createdAt: convertTimestampToDateFormat(history.createdAt),
           title: history.title,
-          change: history.users[uid].change,
-          wallet: history.users[uid].wallet
+          diff: history.users[myUid]?.diff,
+          wallet: history.users[myUid]?.wallet
         })
       })
       return {
@@ -257,15 +281,15 @@ export default defineComponent({
       }
     })
 
+    // history dialog
     const historyDialog = reactive({
       id: '',
       author: '',
       createdAt: '',
       title: '',
       users: [] as {
-        id: string
-        name: string
-        change: number
+        uid: string
+        diff: number
         wallet: number
       }[]
     })
@@ -273,68 +297,97 @@ export default defineComponent({
     const showHistoryDialog = (id: string) => {
       const history = histories.value.filter((history) => history.id === id)[0]
       historyDialog.id = history.id
-      historyDialog.author = group.users[history.author].name
+      historyDialog.author = history.author
       historyDialog.createdAt = convertTimestampToDateTimeFormat(
         history.createdAt
       )
       historyDialog.title = history.title
       historyDialog.users = []
-      Object.keys(history.users).forEach((key) => {
+      Object.keys(history.users).forEach((uid) => {
         historyDialog.users.push({
-          id: key,
-          name: group.users[key].name,
-          change: history.users[key].change,
-          wallet: history.users[key].wallet
+          uid,
+          diff: history.users[uid].diff,
+          wallet: history.users[uid].wallet
         })
       })
       historyDialogModal.value = true
     }
 
+    // add transaction dialog
     const addTransactionDialog = reactive({
       title: '',
       users: [] as {
-        id: string
-        name: string
-        change: string
+        uid: string
+        diff: string
       }[]
     })
     const addTransactionDialogModal = ref(false)
+    const addTransactionDialogLoading = ref(false)
     const showAddTransactionDialog = () => {
       addTransactionDialog.title = ''
       addTransactionDialog.users = []
       Object.keys(group.users).forEach((key) => {
         addTransactionDialog.users.push({
-          id: key,
-          name: group.users[key].name,
-          change: ''
+          uid: key,
+          diff: ''
         })
       })
       addTransactionDialogModal.value = true
     }
-    const adjustDifference = (id: string) => {
+    const adjustDifference = (uid: string) => {
       const user = addTransactionDialog.users.filter(
-        (user) => user.id === id
+        (user) => user.uid === uid
       )[0]
-      user.change = addTransactionDialog.users
-        .filter((user) => user.id !== id)
-        .reduce((total, user) => total - Number(user.change), 0)
+      user.diff = addTransactionDialog.users
+        .filter((user) => user.uid !== uid)
+        .reduce((total, user) => total - Number(user.diff), 0)
         .toString()
     }
     const addTransaction = () => {
+      const users = {} as {
+        [uid: string]: {
+          diff: number
+        }
+      }
+      addTransactionDialog.users.forEach((user) => {
+        if (Number(user.diff) === 0) {
+          return
+        }
+        users[user.uid] = {
+          diff: Number(user.diff)
+        }
+      })
+      // check non-numeric & zero (consistency)
       if (
-        addTransactionDialog.users.reduce(
-          (total, user) => total + Number(user.change),
+        Object.keys(users).reduce(
+          (total, uid) => total + users[uid].diff,
           0
         ) !== 0
       ) {
         window.alert('金額の差が0になるように修正してください。')
         return
       }
-      window.alert('ok')
+      // continue
+      addTransactionDialogLoading.value = true
+      $firebase
+        .app()
+        .functions('asia-northeast1')
+        .httpsCallable('addTransaction')({
+          group: group.id,
+          title: addTransactionDialog.title,
+          users
+        } as IAddTransaction)
+        .then((response) => console.log(response))
+        .catch((error) => console.error(error))
+        .finally(() => {
+          updateGroupState()
+          addTransactionDialogLoading.value = false
+          addTransactionDialogModal.value = false
+        })
     }
 
     return {
-      uid,
+      myUid,
       ready,
       group,
       historiesTable,
@@ -343,6 +396,7 @@ export default defineComponent({
       showHistoryDialog,
       addTransactionDialog,
       addTransactionDialogModal,
+      addTransactionDialogLoading,
       showAddTransactionDialog,
       adjustDifference,
       addTransaction
