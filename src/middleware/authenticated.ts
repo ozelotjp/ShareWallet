3ce8786b7f5589687b2774a4028a6a8b6708a348
ozelotjp <contact@ozelot.app>
2020-04-18 02:43:17 +0000

    fix: データ読み込みを非同期処理から同期処理に変更

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -2,14 +2,18 @@ import { Middleware } from '@nuxt/types'
 import { IGroupDocumentData } from '@@/models/GroupDocument'
 import { groupStore, authenticatedStore } from '@/store'
 
-const myMiddleware: Middleware = ({ redirect, route, app: { $firebase } }) => {
+const myMiddleware: Middleware = async ({
+  redirect,
+  route,
+  app: { $firebase }
+}) => {
   if ($firebase.auth().currentUser === null) {
     authenticatedStore.setNextUrl(route.path)
     redirect('/signin')
     return
   }
   if (groupStore.ready === false) {
-    $firebase
+    const documentsQuery = await $firebase
       .firestore()
       .collection('group')
       .where(`users.${$firebase.auth().currentUser?.uid}.role`, 'in', [
@@ -19,17 +23,15 @@ const myMiddleware: Middleware = ({ redirect, route, app: { $firebase } }) => {
         'read'
       ])
       .get()
-      .then((documentsQuery) => {
-        const group = [] as IGroupDocumentData[]
-        documentsQuery.docs.forEach((document) => {
-          group.push(
-            Object.assign(document.data(), {
-              id: document.id
-            }) as IGroupDocumentData
-          )
-        })
-        groupStore.initializeGroup(group)
-      })
+    const group = [] as IGroupDocumentData[]
+    documentsQuery.docs.forEach((document) => {
+      group.push(
+        Object.assign(document.data(), {
+          id: document.id
+        }) as IGroupDocumentData
+      )
+    })
+    groupStore.initializeGroup(group)
   }
 }
 
