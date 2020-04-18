4601d2c343d2d84d7c6b88f7a2023a69b7649f7e
ozelotjp <contact@ozelot.app>
2020-04-18 03:58:33 +0000

    chore: promiseをasync/awaitに変更

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -30,77 +30,70 @@ export const addTransaction = functions
     const myUid = context.auth?.uid
     const now = admin.firestore.Timestamp.now()
 
-    return admin.firestore().runTransaction((transaction) => {
-      return transaction
-        .get(admin.firestore().collection('group').doc(data.group))
-        .then((groupDocument) => {
-          // check: not found
-          if (groupDocument.exists === false) {
-            throw new Error('not found')
-          }
-          const groupDocumentData = Object.assign(groupDocument.data(), {
-            id: groupDocument.id
-          }) as IGroupDocumentData
+    return admin.firestore().runTransaction(async (transaction) => {
+      const groupDocument = await transaction.get(
+        admin.firestore().collection('group').doc(data.group)
+      )
+      // check: not found
+      if (groupDocument.exists === false) {
+        throw new Error('not found')
+      }
+      const groupDocumentData = Object.assign(groupDocument.data(), {
+        id: groupDocument.id
+      }) as IGroupDocumentData
 
-          // check: access denied
-          if (
-            ['owner', 'moderator', 'write'].includes(
-              groupDocumentData.users[myUid].role || ''
-            ) === false
-          ) {
-            throw new Error('denied')
-          }
+      // check: access denied
+      if (
+        ['owner', 'moderator', 'write'].includes(
+          groupDocumentData.users[myUid].role || ''
+        ) === false
+      ) {
+        throw new Error('denied')
+      }
 
-          // update group document
-          transaction.set(
-            groupDocument.ref,
-            {
-              updatedAt: now,
-              users: (() => {
-                const users = {} as {
-                  [uid: string]: {
-                    wallet: number
-                  }
-                }
-                Object.keys(data.users).forEach((uid) => {
-                  users[uid] = {
-                    wallet:
-                      groupDocumentData.users[uid].wallet + data.users[uid].diff
-                  }
-                })
-                return users
-              })()
-            } as IGroupDocumentData,
-            { merge: true }
-          )
-
-          // add history document
-          groupDocument.ref
-            .collection('histories')
-            .add({
-              author: myUid,
-              createdAt: now,
-              title: data.title,
-              users: (() => {
-                const users = {} as {
-                  [uid: string]: {
-                    diff: number
-                    wallet: number
-                  }
-                }
-                Object.keys(data.users).forEach((uid) => {
-                  users[uid] = {
-                    diff: data.users[uid].diff,
-                    wallet:
-                      groupDocumentData.users[uid].wallet + data.users[uid].diff
-                  }
-                })
-                return users
-              })()
+      // update group document
+      transaction.set(
+        groupDocument.ref,
+        {
+          updatedAt: now,
+          users: (() => {
+            const users = {} as {
+              [uid: string]: {
+                wallet: number
+              }
+            }
+            Object.keys(data.users).forEach((uid) => {
+              users[uid] = {
+                wallet:
+                  groupDocumentData.users[uid].wallet + data.users[uid].diff
+              }
             })
-            .catch((error) => {
-              throw new Error(error)
-            })
-        })
+            return users
+          })()
+        } as IGroupDocumentData,
+        { merge: true }
+      )
+
+      // add history document
+      await groupDocument.ref.collection('histories').add({
+        author: myUid,
+        createdAt: now,
+        title: data.title,
+        users: (() => {
+          const users = {} as {
+            [uid: string]: {
+              diff: number
+              wallet: number
+            }
+          }
+          Object.keys(data.users).forEach((uid) => {
+            users[uid] = {
+              diff: data.users[uid].diff,
+              wallet: groupDocumentData.users[uid].wallet + data.users[uid].diff
+            }
+          })
+          return users
+        })()
+      })
     })
   })
