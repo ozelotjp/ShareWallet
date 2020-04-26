8892b76cbf95860f5bfaafeea742715bbf3081d0
ozelotjp <contact@ozelot.app>
2020-04-26 17:51:56 +0000

    chore: 権限周りのユーティリティを追加

---
+++ src/utils/role.ts
@@ -0,0 +1,15 @@
+import { IRoleKey, IRoleName } from '@@/models/Role'
+
+const roleList = [
+  { key: 'admin', name: '管理者' },
+  { key: 'write', name: '読み書き' },
+  { key: 'read', name: '閲覧のみ' }
+] as { key: IRoleKey; name: IRoleName }[]
+
+export const convertRoleNameFromKey = (roleKey: IRoleKey): IRoleName => {
+  return roleList.filter((role) => role.key === roleKey)[0]?.name
+}
+
+export const convertRoleKeyFromName = (roleName: IRoleName): IRoleKey => {
+  return roleList.filter((role) => role.name === roleName)[0]?.key
+}
