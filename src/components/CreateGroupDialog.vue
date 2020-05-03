e9698adea817bc72b47a9b78c202a80d3783ffd9
ozelotjp <contact@ozelot.app>
2020-05-03 14:07:18 +0000

    feat: グループの作成機能を追加
    fix #6

---
+++ src/components/CreateGroupDialog.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-dialog v-model="show" max-width="512">
+    <v-card>
+      <v-card-title>
+        グループを作成
+      </v-card-title>
+      <v-card-text>
+        <v-text-field v-model="group.name" label="グループ名" />
+        <v-text-field v-model="group.username" label="あなたの名前" />
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer />
+        <v-btn :loading="loading" @click="createGroup">
+          作成
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, reactive } from '@vue/composition-api'
+import { ICreateGroup } from '@@/models/CreateGroup'
+
+export default defineComponent({
+  setup(_, { root: { $firebase, $router } }) {
+    const show = ref(false)
+    const loading = ref(false)
+    const group = reactive({
+      name: '',
+      username: ''
+    })
+
+    const open = () => {
+      show.value = true
+      loading.value = false
+      group.name = ''
+      group.username = ''
+    }
+
+    const createGroup = () => {
+      loading.value = true
+      $firebase
+        .app()
+        .functions('asia-northeast1')
+        .httpsCallable('createGroup')({
+          name: group.name,
+          username: group.username
+        } as ICreateGroup)
+        .then(({ data: { groupId } }) => {
+          show.value = false
+          $router.push(`/group/${groupId}`)
+        })
+        .catch((error) => {
+          console.error({ error })
+        })
+        .finally(() => {
+          loading.value = false
+        })
+    }
+
+    return {
+      show,
+      loading,
+      group,
+      open,
+      createGroup
+    }
+  }
+})
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,10 +4,10 @@
       <v-col v-for="group in groupList" :key="group.id" cols="4">
         <v-card>
           <v-card-title>
-                {{ group.title }}
+            {{ group.title }}
           </v-card-title>
           <v-card-subtitle>
-                残高： {{ group.users[uid].wallet }}円
+            残高： {{ group.users[uid].wallet }}円
           </v-card-subtitle>
           <v-card-actions>
             <v-spacer />
@@ -18,22 +18,60 @@
         </v-card>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col cols="4">
+        <v-card color="grey lighten-1">
+          <v-card-title>
+            グループを作成
+          </v-card-title>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn @click="showCreateGroupDialog">
+              作成
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <create-group-dialog ref="createGroupDialog" />
   </v-container>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from '@vue/composition-api'
+import {
+  defineComponent,
+  computed,
+  reactive,
+  ref,
+  Ref
+} from '@vue/composition-api'
 import { groupStore } from '@/store'
+import CreateGroupDialog from '@/components/CreateGroupDialog.vue'
 
 export default defineComponent({
   middleware: 'authenticated',
+  components: {
+    CreateGroupDialog
+  },
   setup(_, { root: { $firebase } }) {
     const groupList = computed(() => groupStore.list)
     const uid = $firebase.auth().currentUser!.uid
+    const group = reactive({
+      name: '',
+      username: ''
+    })
+
+    const createGroupDialog = ref() as Ref<any>
+    const showCreateGroupDialog = () => {
+      createGroupDialog.value.open()
+    }
 
     return {
       groupList,
-      uid
+      uid,
+      group,
+      createGroupDialog,
+      showCreateGroupDialog
     }
   }
 })
