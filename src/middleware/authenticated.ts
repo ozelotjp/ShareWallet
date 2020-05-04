7a9fd1b0e22179d40356609206b2b6b14221d328
ozelotjp <contact@ozelot.app>
2020-05-04 00:26:46 +0000

    chore: group collectionの初期化をonSnapshotに統一
    fix #17

--- src/middleware/authenticated.ts
+++ src/middleware/authenticated.ts
@@ -2,18 +2,14 @@ import { Middleware } from '@nuxt/types'
 import { IRoleKey } from '@@/models/Role'
 import { groupStore, authenticatedStore } from '@/store'
 
-const myMiddleware: Middleware = async ({
-  redirect,
-  route,
-  app: { $firebase }
-}) => {
+const myMiddleware: Middleware = ({ redirect, route, app: { $firebase } }) => {
   if ($firebase.auth().currentUser === null) {
     authenticatedStore.setNextUrl(route.path)
     redirect('/signin')
     return
   }
   if (groupStore.ready === false) {
-    const querySnapshot = await $firebase
+    $firebase
       .firestore()
       .collection('group')
       .where(`users.${$firebase.auth().currentUser?.uid}.role`, 'in', [
@@ -21,22 +17,20 @@ const myMiddleware: Middleware = async ({
         'write',
         'read'
       ] as IRoleKey[])
-      .get()
-
-    querySnapshot.query.onSnapshot((document) => {
-      document.docChanges().forEach((change) => {
-        switch (change.type) {
-          case 'added':
-          case 'modified':
-            groupStore.updateGroup(change.doc)
-            break
-          case 'removed':
-            groupStore.removeGroup(change.doc)
-            break
-        }
+      .onSnapshot((document) => {
+        document.docChanges().forEach((change) => {
+          switch (change.type) {
+            case 'added':
+            case 'modified':
+              groupStore.updateGroup(change.doc)
+              break
+            case 'removed':
+              groupStore.removeGroup(change.doc)
+              break
+          }
+        })
+        groupStore.updateReady(true) // 重複して実行されるのは許容
       })
-      groupStore.updateReady(true) // 重複して実行されるのは許容
-    })
   }
 }
 
